9a2ed8deb0ed6abb94b9672db48ebb107448bf1d
ESSS4656 <sebastiansalgado.accenture@itau.cl>
2023-04-28 11:03:42 -0400

    initial base config commit

---
+++ app/src/main/java/com/sebasalgado/loginfirebaseprototype/ui/fragments/FragmentLogin.kt
@@ -0,0 +1,23 @@
+package com.sebasalgado.loginfirebaseprototype.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sebasalgado.loginfirebaseprototype.R
+
+class FragmentLogin : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

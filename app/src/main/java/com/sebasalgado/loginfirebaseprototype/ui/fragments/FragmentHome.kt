b67bb43a2f0b0dd3b9b34f5e4dffebd17cce33f5
Sebastian Salgado <salgadoseba89@gmail.com>
2023-05-15 00:55:02 -0400

    add button navigation logic and binding

--- app/src/main/java/com/sebasalgado/loginfirebaseprototype/ui/fragments/FragmentHome.kt
+++ app/src/main/java/com/sebasalgado/loginfirebaseprototype/ui/fragments/FragmentHome.kt
@@ -5,12 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import com.sebasalgado.loginfirebaseprototype.R
 import com.sebasalgado.loginfirebaseprototype.databinding.FragmentHomeBinding
-import com.sebasalgado.loginfirebaseprototype.databinding.FragmentLoginBinding
-
 class FragmentHome : Fragment() {
 
     private val binding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
@@ -18,25 +14,22 @@ class FragmentHome : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-        binding.buttonLogin.setOnClickListener {
+        binding.includeHome.buttonLogin.setOnClickListener {
             val action = FragmentHomeDirections.actionFragmentHomeToFragmentLogin()
             findNavController().navigate(action)
         }
 
-        binding.buttonRegister.setOnClickListener {
+        binding.includeHome.buttonRegister.setOnClickListener {
             val action = FragmentHomeDirections.actionFragmentHomeToFragmentRegister()
             findNavController().navigate(action)
         }
-
-
 
     }
 }

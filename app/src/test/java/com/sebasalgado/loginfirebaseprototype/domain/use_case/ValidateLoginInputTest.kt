d42393cf1669b0e295e90ae1d4f058b43ba955d7
Sebastian Salgado <salgadoseba89@gmail.com>
2023-05-22 02:44:05 -0400

    create test for validation inputs in the login

---
+++ app/src/test/java/com/sebasalgado/loginfirebaseprototype/domain/use_case/ValidateLoginInputTest.kt
@@ -0,0 +1,35 @@
+package com.sebasalgado.loginfirebaseprototype.domain.use_case
+
+import com.sebasalgado.loginfirebaseprototype.domain.model.LogingInputValidationType
+import org.junit.Test
+import org.junit.Assert.*
+
+class ValidateLoginInputTest {
+
+    private val validateLoginInputUseCase = ValidateLoginInputUseCase()
+
+    @Test
+    fun testEmptyFieldReturnsValidationTypeEmptyField() {
+        val result = validateLoginInputUseCase(email = "", password = "password")
+        assertEquals(result, LogingInputValidationType.EmplyField)
+    }
+
+    @Test
+    fun testEmptyPasswordFieldReturnsValidationTypeEmptyField() {
+        val result = validateLoginInputUseCase("corre@gmail.com", "")
+        assertEquals(result, LogingInputValidationType.EmplyField)
+    }
+
+    @Test
+    fun testNoEmailFieldReturnsTrueValidationTypeNoEmailField() {
+        val result = validateLoginInputUseCase("correogmail.com", "password")
+        assertEquals(result, LogingInputValidationType.NoEmail)
+    }
+
+    @Test
+    fun testValidFieldReturnsValidationTypeField() {
+        val result = validateLoginInputUseCase("correo@gmail.com", "password")
+        assertEquals(result, LogingInputValidationType.Valid)
+    }
+
+}

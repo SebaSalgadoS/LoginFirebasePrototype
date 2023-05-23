7876bc37b1cfb0ee20447957f71f586a391ec347
ESSS4656 <sebastiansalgado.accenture@itau.cl>
2023-05-23 12:47:25 -0400

    create String extension for validation types

--- app/src/main/java/com/sebasalgado/loginfirebaseprototype/utils/extentions/StringExtension.kt
+++ app/src/main/java/com/sebasalgado/loginfirebaseprototype/utils/extentions/StringExtension.kt
@@ -1,2 +1,16 @@
 package com.sebasalgado.loginfirebaseprototype.utils.extentions
 
+fun String.containsNumbers(): Boolean{
+    val regex = Regex(".*\\d+.*")
+    return regex.matches(this)
+}
+
+fun String.constainsUpperCase(): Boolean{
+    val regex = Regex(".*[A-Z]+.*")
+    return regex.matches(this)
+}
+
+fun String.constainsSpecialChar(): Boolean{
+    val regex = Regex(".*[^A-Za-z\\d]+.*")
+    return regex.matches(this)
+}
